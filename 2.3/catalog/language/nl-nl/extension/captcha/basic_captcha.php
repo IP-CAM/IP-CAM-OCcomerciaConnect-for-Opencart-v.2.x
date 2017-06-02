67ca1e9979dd1bb318ae49ff0c2da66d789826be
Mark Smit <m.smit@hostingondemand.nl>
2017-06-02 15:17:43 +0200

    new

--- 2.3/catalog/language/nl-nl/extension/captcha/basic_captcha.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Text
-$_['text_captcha'] = 'Captcha';
-
-// Entry
-$_['entry_captcha'] = 'Voer de Verificatiecode in de onderstaande box in';
-
-// Error
-$_['error_captcha'] = 'De Verificatiecode komt niet overeen!';
