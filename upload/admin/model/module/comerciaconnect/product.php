4ee783e9617e2045a262733d694c45d4c15b1afa
Maxim Janssens <maxim@mpjconsultancy.nl>
2017-10-18 15:21:06 +0200

    CC-155 -- 1.5.2.1 Compatibility fixes.

--- upload/admin/model/module/comerciaconnect/product.php
+++ upload/admin/model/module/comerciaconnect/product.php
@@ -234,7 +234,7 @@ class ModelModuleComerciaconnectProduct extends Model
         $this->db->query("UPDATE `" . DB_PREFIX . "category` SET `ccHash` = '" . $this->getHashForCategory($category) . "' WHERE `category_id` = '" . $category['category_id'] . "'");
     }
 
-    // Start OC Version <= 1.5.2 specific functions
+    // Start OC Version <= 1.5.2.1 specific functions
     public function getOptionValue($option_value_id)
     {
         return $this->db->query("SELECT * FROM " . DB_PREFIX . "option_value ov LEFT JOIN " . DB_PREFIX . "option_value_description ovd ON (ov.option_value_id = ovd.option_value_id) WHERE ov.option_value_id = '" . (int)$option_value_id . "' AND ovd.language_id = '" . (int)$this->config->get('config_language_id') . "'")->row;
@@ -244,7 +244,7 @@ class ModelModuleComerciaconnectProduct extends Model
     {
         return $this->db->query("SELECT DISTINCT *, (SELECT keyword FROM " . DB_PREFIX . "url_alias WHERE query = 'category_id=" . (int)$category_id . "') AS keyword FROM " . DB_PREFIX . "category c JOIN " . DB_PREFIX . "category_description cd ON (cd.category_id = c.category_id) WHERE c.category_id = '" . (int)$category_id . "'")->row;
     }
-    // End OC Version <= 1.5.2 specific functions
+    // End OC Version <= 1.5.2.1 specific functions
 
 }
 ?>
