06815d3763bbfadf84fa737605c66ce0851ebf87
Alen Androsevic <alenandrosevic@gmail.com>
2017-11-30 10:51:16 +0100

    CC-100 -- Changed wherefield from table to type & wrapped  deactivation-batch in a conditional block

--- upload/admin/model/ccSync/7_cleanup.php
+++ upload/admin/model/ccSync/7_cleanup.php
@@ -1,29 +1,28 @@
 <?php
 use comercia\Util;
 use comerciaConnect\logic\Product;
-use comerciaConnect\logic\Website;
 
 class ModelCcSync7Cleanup extends Model
 {
     public function sync($data)
     {
         $deletedProducts = Util::db()->select('ccDeletedEntities', [], [
-            'table' => 'product',
+            'type' => 'product',
             'isCleaned' => '0'
         ]);
 
-        foreach ($deletedProducts as $product)
-        {
-            $batch[] = $product['entityId'];
+        if (count($deletedProducts)) {
+            foreach ($deletedProducts as $product) {
+                $batch[] = $product['entityId'];
+            }
+
+            Product::deactivateBatch($data->session, $batch);
+
+            foreach ($deletedProducts as &$product) {
+                $product['isCleaned'] = 1;
+            }
+
+            Util::db()->saveDataObjectArray('ccDeletedEntities', $deletedProducts);
         }
-
-        Product::deactivateBatch($data->session, $batch);
-
-        foreach ($deletedProducts as &$product)
-        {
-            $product['isCleaned'] = 1;
-        }
-
-        Util::db()->saveDataObjectArray('ccDeletedEntities', $deletedProducts);
     }
 }
