cbbf4cff031a8296ff0ec80ad07690d1422b41a6
Mark Smit <m.smit@hostingondemand.nl>
2018-06-08 17:42:47 +0200

    2.0.1--fix triggers

--- upload/admin/patch/comerciaConnect/4_addDeletedEntityTable.php
+++ upload/admin/patch/comerciaConnect/4_addDeletedEntityTable.php
@@ -8,7 +8,7 @@ return function () {
         ->addField('isCleaned', 'tinyint')
         ->save();
 
-    Util::db()->query("CREATE TRIGGER ccDeleteProduct AFTER DELETE ON `" . DB_PREFIX . "product` FOR EACH ROW BEGIN INSERT INTO `" . DB_PREFIX . "ccDeletedEntities` SET `type` = 'product', `entityId` = old.product_id, `isCleaned` = 0; END");
-    Util::db()->query("CREATE TRIGGER ccDeleteOrder AFTER DELETE ON `" . DB_PREFIX . "order` FOR EACH ROW BEGIN INSERT INTO `" . DB_PREFIX . "ccDeletedEntities` SET `type` = 'order', `entityId` = old.order_id, `isCleaned` = 0; END");
-    Util::db()->query("CREATE TRIGGER ccDeleteCategory AFTER DELETE ON `" . DB_PREFIX . "category` FOR EACH ROW BEGIN INSERT INTO `" . DB_PREFIX . "ccDeletedEntities` SET `type` = 'category', `entityId` = old.category_id, `isCleaned` = 0; END");
+    Util::db()->query("DROP TRIGGER IF EXISTS `ccDeleteProduct`;CREATE TRIGGER ccDeleteProduct AFTER DELETE ON `" . DB_PREFIX . "product` FOR EACH ROW BEGIN INSERT INTO `" . DB_PREFIX . "ccDeletedEntities` SET `type` = 'product', `entityId` = old.product_id, `isCleaned` = 0; END");
+    Util::db()->query("DROP TRIGGER IF EXISTS `ccDeleteOrder`;CREATE TRIGGER ccDeleteOrder AFTER DELETE ON `" . DB_PREFIX . "order` FOR EACH ROW BEGIN INSERT INTO `" . DB_PREFIX . "ccDeletedEntities` SET `type` = 'order', `entityId` = old.order_id, `isCleaned` = 0; END");
+    Util::db()->query("DROP TRIGGER IF EXISTS `ccDeleteCategory`;CREATE TRIGGER ccDeleteCategory AFTER DELETE ON `" . DB_PREFIX . "category` FOR EACH ROW BEGIN INSERT INTO `" . DB_PREFIX . "ccDeletedEntities` SET `type` = 'category', `entityId` = old.category_id, `isCleaned` = 0; END");
 };
