ad9c2eecea7195288badf19437e96126841e0288
Kim Hoogenberg <k.hoogenberg@comercia.nl>
2018-06-15 15:54:01 +0200

    SCM - deleted unwanted files

--- upload/admin/patch/comerciaConnect/comerciaConnect/4_addDeletedEntityTableProductCategory.php
+++
@@ -1,6 +0,0 @@
-<?php
-use comercia\Util;
-
-return function () {
-    Util::db()->query("DROP TRIGGER IF EXISTS `ccDeleteProductCategory`;CREATE TRIGGER ccDeleteProductCategory AFTER DELETE ON `" . DB_PREFIX . "product_to_category` FOR EACH ROW BEGIN INSERT INTO `" . DB_PREFIX . "ccDeletedEntities` SET `type` = 'prodductCategory', `entityId` = concat(old.product_id,'_',old.category_id), `isCleaned` = 0; END");
-};
--- upload/admin/patch/comerciaConnect/comerciaConnect/7_addDeletedEntityStore.php
+++
@@ -1,7 +0,0 @@
-<?php
-use comercia\Util;
-
-return function () {
-    Util::db()->query("DROP TRIGGER IF EXISTS `ccDeleteProductStore`;CREATE TRIGGER ccDeleteProductStore AFTER DELETE ON `" . DB_PREFIX . "product_to_store` FOR EACH ROW BEGIN INSERT INTO `" . DB_PREFIX . "ccDeletedEntities` SET `type` = 'prodductStore', `entityId` = concat(old.product_id,'_',old.store_id), `isCleaned` = 0; END");
-    Util::db()->query("DROP TRIGGER IF EXISTS `ccDeleteCategoryStore`;CREATE TRIGGER ccDeleteCategoryStore AFTER DELETE ON `" . DB_PREFIX . "category_to_store` FOR EACH ROW BEGIN INSERT INTO `" . DB_PREFIX . "ccDeletedEntities` SET `type` = 'categoryStore', `entityId` = concat(old.category_id,'_',old.store_id), `isCleaned` = 0; END");
-};
