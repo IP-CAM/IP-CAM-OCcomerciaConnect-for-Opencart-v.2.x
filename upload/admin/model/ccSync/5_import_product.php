de4d870dc0d436d84b71604996d1a3b72b56560e
mark <m.smit@hostingondemand.nl>
2018-10-23 15:56:19 +0300

    CC-314 -- fetch data from cc in batches too

--- upload/admin/model/ccSync/5_import_product.php
+++ upload/admin/model/ccSync/5_import_product.php
@@ -1,4 +1,5 @@
 <?php
+
 use comerciaConnect\logic\Product;
 
 class ModelCcSync5ImportProduct extends Model
@@ -9,18 +10,35 @@ class ModelCcSync5ImportProduct extends Model
         $filter->filter("lastTouchedBy", TOUCHED_BY_API, "!=");
         $filter->filter("type", PRODUCT_TYPE_PRODUCT);
         $filter->filter("parent_product_id", "empty", "=");
-        $products = $filter->getData();
+        $filter->filter("limit", CC_BATCH_SIZE);
 
-        \comerciaConnect\lib\Debug::writeMemory("Received product data");
+        $lastResultHash = false;
 
-        foreach ($products as $product) {
-            $data->ccProductModel->saveProduct($product);
-            \comerciaConnect\lib\Debug::writeMemory("Saved product ".$product->id);
+        while ($products = $filter->getData()) {
+            if (empty($products)) {
+                break;
+            }
+
+            $resultHash = md5(print_r($products));
+
+            //break if same data is fetched, Most probably something went wrong
+            if ($resultHash == $lastResultHash) {
+                \comerciaConnect\lib\Debug::writeMemory("Error: Same result hash and last result hash");
+                break;
+            }
+
+            \comerciaConnect\lib\Debug::writeMemory("Received product data");
+
+            foreach ($products as $product) {
+                $data->ccProductModel->saveProduct($product);
+                \comerciaConnect\lib\Debug::writeMemory("Saved product " . $product->id);
+            }
+
+            $data->ccProductModel->touchBatch($data->session, $products);
+
+            \comerciaConnect\lib\Debug::writeMemory("Sent product touched by batch");
+            $lastResultHash = $resultHash;
         }
-
-        $data->ccProductModel->touchBatch($data->session,$products);
-
-        \comerciaConnect\lib\Debug::writeMemory("Sent product touched by batch");
 
     }
 }
